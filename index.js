9356ea7e5eae52896d4025e6460a299165733760
Daniel Koller <daniel-koller@gmx.at>
2023-01-10 18:11:22 +0100

    Implement Random Color Generator

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-import emoji from 'node-emoji';
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
 
-const chosenEmoji = process.argv[2];
+// Default color options for randomColor
+const colorOptions = {
+  luminosity: 'random',
+  hue: 'random',
+};
 
-if (!chosenEmoji) {
-  console.log(emoji.random().emoji);
-} else if (emoji.hasEmoji(chosenEmoji)) {
-  console.log(emoji.get(chosenEmoji));
-} else {
-  console.log('Emoji not found');
+// Parse command line arguments and check if the input value is valid. If not, a random color is put out
+const args = process.argv.slice(2);
+if (args.length > 0) {
+  const hues = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'purple',
+    'pink',
+    'monochrome',
+  ];
+  if (hues.some((hue) => args[0] === hue)) {
+    colorOptions.hue = args[0];
+  } else {
+    console.log('Invalid choice, Output goes back to Random');
+  }
 }
+
+if (args.length > 1) {
+  const lums = ['bright', 'light', 'dark'];
+  if (lums.some((lum) => args[1] === lum)) {
+    colorOptions.hue = args[1];
+  } else {
+    console.log('Invalid choice, Output goes back to Random');
+  }
+}
+
+// Generate random color
+const color = randomColor(colorOptions);
+
+// Console.log the colored block with the hex value inside
+function colorAndLog(str) {
+  console.log(chalk.hex(color).bold(str));
+}
+
+colorAndLog(`##############################
+##############################
+##############################
+########              ########
+########   ${color}    ########
+########              ########
+##############################
+##############################
+##############################`);
