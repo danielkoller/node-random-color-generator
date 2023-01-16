67a70c3fb390bbbb4b2c83d41b83f426e4e97fad
Daniel Koller <daniel-koller@gmx.at>
2023-01-16 17:45:30 +0100

    Simplify and remove hard coded options

--- index.js
+++ index.js
@@ -7,34 +7,12 @@ const colorOptions = {
   hue: 'random',
 };
 
-// Parse command line arguments and check if the input value is valid. If not, a random color is put out
+// Slices the input
 const args = process.argv.slice(2);
-if (args.length > 0) {
-  const hues = [
-    'red',
-    'orange',
-    'yellow',
-    'green',
-    'blue',
-    'purple',
-    'pink',
-    'monochrome',
-  ];
-  if (hues.some((hue) => args[0] === hue)) {
-    colorOptions.hue = args[0];
-  } else {
-    console.log('Invalid choice, Output goes back to Random');
-  }
-}
-
-if (args.length > 1) {
-  const lums = ['bright', 'light', 'dark'];
-  if (lums.some((lum) => args[1] === lum)) {
-    colorOptions.luminosity = args[1];
-  } else {
-    console.log('Invalid choice, Output goes back to Random');
-  }
-}
+// Gets the input for the color
+colorOptions.hue = args[0];
+// Gets the input for the luminosity
+colorOptions.luminosity = args[1];
 
 // Generate random color
 const color = randomColor(colorOptions);
